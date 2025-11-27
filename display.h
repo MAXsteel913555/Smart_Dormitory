ed7e200f7ad860c19a148cccb21ac03b6cce1c5e
MAXsteel913555 <2382828997@qq.com>
2025-11-27 15:33:04 +0800

    Add display.h with function declarations

---
+++ display.h
@@ -0,0 +1,13 @@
+#ifndef __DISPLAY_H
+#define __DISPLAY_H
+
+#include "common.h"
+
+// 函数声明
+void Display_Current_Screen(void);
+void Display_Main_Menu(void);
+void Display_Temp_Setting(void);
+void Display_Humid_Setting(void);
+void Display_Smoke_Setting(void);
+
+#endif
